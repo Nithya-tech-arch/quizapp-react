5e5b5c9fe793d355a91257f52ce84a5a40982e9d
Nithyasree <nithya2426845@gmail.com>
2021-06-28 16:16:18 +0530

    quizapp with reactjs

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./assets/style.css";
+import quizService from "./quizService";
+import QuestionBox from "./components/QuestionBox";
+class Quizapp extends Component {
+    state = {
+        questionBank: []
+    };
+    getQuestions = () =>{
+        quizService().then(question => {
+            this.setState({
+                questionBank: question
+            });
+        });
+    };
+    componentDidMount() {
+        this.getQuestions();
+    }
+
+
+    render () {
+        return(
+            <div className="container">
+                <div className="title">QuizBee</div>
+                {this.state.questionBank.length > 0 && this.state.questionBank.map(
+                    ({question, answers, correct, questionId}) => ( 
+                    <QuestionBox question={question} options={answers} key={questionId} />
+                    )
+                )} 
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Quizapp />, document.getElementById("root"));
